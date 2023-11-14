c0998ab61404e8f917a8b1929152f31ddfe6c1d0
nebulous42069 <125807561+nebulous42069@users.noreply.github.com>
2023-11-13 21:13:37 -0500

    update

--- nexus/plugin.video.nhlstreams/service_entry.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Service entry point"""
-
-from __future__ import absolute_import, division, unicode_literals
-
-if __name__ == '__main__':
-    from resources.lib.service import run
-
-    run()
