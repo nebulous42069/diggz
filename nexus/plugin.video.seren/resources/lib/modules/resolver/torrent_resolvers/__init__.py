608cd3332508f9154dd58ae45267f2730356430f
nebulous42069 <125807561+nebulous42069@users.noreply.github.com>
2024-03-09 13:27:56 -0500

    seren

--- nexus/plugin.video.seren/resources/lib/modules/resolver/torrent_resolvers/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["PremiumizeResolver", "RealDebridResolver", "AllDebridResolver"]
-
-from .all_debrid import AllDebridResolver
-from .premiumize import PremiumizeResolver
-from .real_debrid import RealDebridResolver
